61c8057cd7cb707d352de8a01d5e45d2d24c6146
Darlan Caruso <devcaruso@gmail.com>
2019-05-01 20:44:25 -0300

    Password bcrypt

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,12 @@
 const express = require('express')
-
 const routes = express.Router()
+
+const UserController = require('./app/controllers/UserController')
 
 routes.get('/', (req, res) => {
   return res.send('Hello world!')
 })
+
+routes.post('/users', UserController.store)
 
 module.exports = routes
