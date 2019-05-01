8a0cfbef1bdc5d6d0ce3cad04ad902c69ca7e9bf
Darlan Caruso <devcaruso@gmail.com>
2019-05-01 20:56:44 -0300

    Adding JWT

--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,13 @@ const express = require('express')
 const routes = express.Router()
 
 const UserController = require('./app/controllers/UserController')
+const SessionController = require('./app/controllers/SessionController')
 
 routes.get('/', (req, res) => {
   return res.send('Hello world!')
 })
 
 routes.post('/users', UserController.store)
+routes.post('/login', SessionController.store)
 
 module.exports = routes
