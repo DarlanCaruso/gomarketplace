895ca9cae338dcb0f57bd7f0761f6d279880342e
Darlan Caruso <devcaruso@gmail.com>
2019-05-04 10:56:18 -0300

    Creating purchase controller and send mails

---
+++ src/app/controllers/PurchaseController.js
@@ -0,0 +1,23 @@
+const Ad = require('../models/Ad')
+const User = require('../models/User')
+const Mail = require('../services/Main')
+
+class PurchaseController {
+  async store (req, res) {
+    const { ad, content } = req.body
+
+    const purchaseAd = await Ad.findById(ad).populate('author')
+    const user = await User.findById(req.userId)
+
+    Mail.sendMail({
+      form: '"Diego Fernandes" <devcaruso@gmail.com>',
+      to: purchaseAd.author.email,
+      subject: `Solicitação de compra: ${purchaseAd.title}`,
+      html: `<p>${content}</p>`
+    })
+
+    return res.send()
+  }
+}
+
+module.exports = new PurchaseController()
---
+++ src/config/mail.js
@@ -0,0 +1,9 @@
+module.exports = {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '5aafde20af4184',
+    pass: '729f0d36aaf1a3'
+  }
+}
